7438d082834c449e7e39b8d8f533daf55a7c36b3
Khanimambo Maluleke <khanimambomaluleke@gmail.com>
2024-10-08 09:40:34 +0200

    Enter number of Bookings

--- src/Bookings.java
+++ src/Bookings.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Bookings {
     public static void main(String[] args) {
-        System.out.println("Hello Bookings");
+
+        System.out.println("Enter the number of bookings");
+        Scanner input = new Scanner(System.in);
+
+        int totalBookings = input.nextInt();
     }
 }
