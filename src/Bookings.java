ab965f74c1ec26a44800244b5cdc65b12fa6bdc1
Khanimambo Maluleke <khanimambomaluleke@gmail.com>
2024-10-09 09:54:46 +0200

    Enter bookings times

--- src/Bookings.java
+++ src/Bookings.java
@@ -7,5 +7,20 @@ public class Bookings {
         Scanner input = new Scanner(System.in);
 
         int totalBookings = input.nextInt();
+
+        System.out.println("Enter the bookings, one per line:");
+
+        int i = 1;
+
+
+        while( i <= totalBookings) {
+            String bookingInstance = input.nextLine();
+            String newBook = bookingInstance.replace(" ", "");
+
+            if (newBook.length() == 0) {
+                continue;
+            }
+            i++;
+        }
     }
 }
